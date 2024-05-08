4a611e66422f006e3c15c9c59cd6bbf45e36701e
leonnysantos <leonnypat@gmail.com>
2024-05-07 21:42:31 -0300

    Login com JSON Web Token

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,8 @@ import { sequelize } from '../database'
 import { DataTypes, Model, Optional } from 'sequelize'
 import bcrypt from 'bcrypt'
 
+type CheckPasswordCallback = (err?: Error, isSame?: boolean) => void
+
 export interface User {
     id: number
     firstName: string
@@ -13,11 +15,11 @@ export interface User {
     role: 'admin' | 'user'
 }
 
-export interface UserCreationAttributes
-    extends Optional<User, 'id'> { }
+export interface UserCreationAttributes extends Optional<User, 'id'> { }
 
-export interface UserInstance
-    extends Model<User, UserCreationAttributes>, User { }
+export interface UserInstance extends Model<User, UserCreationAttributes>, User {
+    checkPassword: (password: string, callbackfn: CheckPasswordCallback) => void
+}
 
 export const User = sequelize.define<UserInstance, User>('users', {
     id: {
@@ -67,3 +69,13 @@ export const User = sequelize.define<UserInstance, User>('users', {
         }
     }
 })
+
+User.prototype.checkPassword = function (password: string, callbackfn: CheckPasswordCallback) {
+    bcrypt.compare(password, this.password, (err, isSame) => {
+        if (err) {
+            callbackfn(err)
+        } else {
+            callbackfn(err, isSame)
+        }
+    })
+}
