afc8ce85553f565023c9cc5dbf3a0c88bc9df3e0
leonnysantos <leonnypat@gmail.com>
2024-05-10 20:52:26 -0300

    Atualizando informações do usuário

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -80,5 +80,17 @@ export const userService = {
         // @ts-ignore
         keepWatchingList.sort((a, b) => a.watchTime.updatedAt < b.watchTime.updatedAt ? 1 : -1)
         return keepWatchingList
+    },
+
+    update: async (id: number, attributes: {
+        firstName: string
+        lastName: string
+        phone: string
+        birth: Date
+        email: string
+    }) => {
+        const [affectedRows, updatedUsers] = await User.update(attributes, { where: { id }, returning: true })
+
+        return updatedUsers[0]
     }
 }
